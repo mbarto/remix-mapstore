d9e1770bcacc2c2c6abafd90ca550b84bccb5746
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2022-02-05 14:28:43 +0100

    fixed translations loading to work in production

--- app/root.tsx
+++ app/root.tsx
@@ -24,9 +24,13 @@ export let links: LinksFunction = () => {
 }
 
 export const loader: LoaderFunction = async ({ request }) => {
+    const url = new URL(request.url)
+
     const [language] = request.headers.get("Accept-Language").split(",")
     const i18n = await (
-        await fetch(`http://localhost:3000/translations/data.${language}.json`)
+        await fetch(
+            `${url.protocol}://${url.host}/translations/data.${language}.json`
+        )
     ).json()
 
     return {
